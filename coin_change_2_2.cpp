e5c41a1eb90c2a599b62e3c2d40f0cbd02abc0ba
Vũ Quang Thắng <thangbienhoa3gmail.com@MacBook-Pro-cua-Vu.local>
2024-09-12 19:15:42 +0700

    first commit

---
+++ coin_change_2_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+int n;
+int a[100];
+int dp[100];
+
+int main()
+{
+    int s;
+    cin >> n >> s;
+    for(int i = 0;i < n;i++) cin >> a[i];
+    dp[0] = 1;
+    for(int i = 0;i < n;i++)
+    {
+        for(int j = a[i];j <= s;j++)
+        {
+            dp[j] += dp[j - a[i]];
+        }
+    }
+    cout << dp[s] << endl;
+}
